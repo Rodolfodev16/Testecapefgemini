256326f746ff8a09e8d5525467fb4b17ee41e2f1
Rodolfodev16 <98677029+Rodolfodev16@users.noreply.github.com>
2022-02-20 18:47:04 -0300

    Add files via upload

---
+++ Questao1.java
@@ -0,0 +1,11 @@
+public class Questao1 {
+    public static void main(String args[]) {
+
+        System.out.print("* * *\n"
+                +                "    *\n"
+                +                "    * * *\n"
+                +                "        *\n"
+                +                "        *\n");
+    }
+}
+
